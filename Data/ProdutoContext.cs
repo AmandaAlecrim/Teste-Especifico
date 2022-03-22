731419092f32cfc89cac8da7e4aace225f230c4a
AmandaAlecrim <amanda2668@hotmail.com>
2022-03-22 00:27:34 -0300

    Alterando tabela e colunas no banco

--- Data/ProdutoContext.cs
+++ Data/ProdutoContext.cs
@@ -10,5 +10,18 @@ namespace desafio.Data
         }
 
         public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var produto = modelBuilder.Entity<Produto>();
+
+            produto.ToTable("tb_produto");
+            produto.HasKey(x => x.Id);// definindo chave primária da tabela
+
+            //definindo nome das colunas da tabela
+            produto.Property(x => x.Id).HasColumnName("id").ValueGeneratedOnAdd();
+            produto.Property(x => x.NomeProduto).HasColumnName("nome_produto").IsRequired();
+            produto.Property(x => x.Preco).HasColumnName("preco");
+        }
     }
 }
---
+++ Migrations/20220322030634_AtualizacaoProduto.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace desafio.Migrations
+{
+    public partial class AtualizacaoProduto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Produtos",
+                table: "Produtos");
+
+            migrationBuilder.RenameTable(
+                name: "Produtos",
+                newName: "tb_produto");
+
+            migrationBuilder.RenameColumn(
+                name: "Preco",
+                table: "tb_produto",
+                newName: "preco");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "tb_produto",
+                newName: "id");
+
+            migrationBuilder.RenameColumn(
+                name: "NomeProduto",
+                table: "tb_produto",
+                newName: "nome_produto");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_tb_produto",
+                table: "tb_produto",
+                column: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_tb_produto",
+                table: "tb_produto");
+
+            migrationBuilder.RenameTable(
+                name: "tb_produto",
+                newName: "Produtos");
+
+            migrationBuilder.RenameColumn(
+                name: "preco",
+                table: "Produtos",
+                newName: "Preco");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Produtos",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "nome_produto",
+                table: "Produtos",
+                newName: "NomeProduto");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Produtos",
+                table: "Produtos",
+                column: "Id");
+        }
+    }
+}
